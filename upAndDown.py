d2f8b16d0f3bbf9e656ad50a2b075f127a37c936
Davis Arrizqi Putra Mandiri <85699695+davisapplication@users.noreply.github.com>
2022-11-09 22:06:05 +0700

    Up And Down - Python Principles Challenges

---
+++ upAndDown.py
@@ -0,0 +1,9 @@
+def up_down(number):
+    # made it up!
+    underNumber = number - 1
+    aboveNumber = number + 1
+    
+    # take it into a tuple
+    tupleResult = (underNumber, aboveNumber)
+    
+    return tupleResult
